7c93282738fc7d2cb8e51bc351ee3f87af8050ca
Lucas G. Stopassolli <65601519+lucasgstopassolli@users.noreply.github.com>
2025-11-12 15:11:56 -0300

    First commit

--- Desenvolvimento Web I/Trabalho Final/public/index.php
+++ Desenvolvimento Web I/Trabalho Final/public/index.php
@@ -8,14 +8,17 @@
 
     <!-- Style -->
     <link rel="stylesheet" href="css/library/bootstrap.min.css">
+    <link rel="stylesheet" href="css/library/splide.min.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
 
+    
 
     <!-- Script -->
     <script src="js/library/bootstrap.bundle.min.js"></script>
+    <script src="js/library/splide.min.js"></script>
     <script src="js/scripts.js"></script>
 </body>
 
