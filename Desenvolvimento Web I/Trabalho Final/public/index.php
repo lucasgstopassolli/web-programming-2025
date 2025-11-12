73ad2824338ffceafc9ae54ae77a50f42a6131fc
Lucas G. Stopassolli <lucasgstopassolli@gmail.com>
2025-11-12 08:29:21 -0300

    Final Activitie Update

---
+++ Desenvolvimento Web I/Trabalho Final/public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avaliação de Satisfação</title>
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/library/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+
+    <!-- Script -->
+    <script src="js/library/bootstrap.bundle.min.js"></script>
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
